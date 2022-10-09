fb2b41e36029f21fc48f1b340ab65b7e3f1c6873
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2022-10-09 20:12:07 -0300

    create login presenter

---
+++ cine-room/Modules/Login/LoginPresenter.swift
@@ -0,0 +1,18 @@
+//
+//  LoginPresenter.swift
+//  cine-room
+//
+//  Created by Alexandre Cardoso on 09/10/22.
+//
+
+import UIKit
+
+final class LoginPresenter {
+
+    private weak var view: UIViewController?
+
+    init(view: UIViewController) {
+        self.view = view
+    }
+
+}
